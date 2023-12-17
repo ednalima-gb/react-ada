c6bad1313cebe24c61372d40e731e4536e4625d8
ednalima-gb <edna.lima@grupoboticario.com.br>
2023-12-17 11:09:55 -0300

    inserindo nestjs

--- aula2/src/components/Card.jsx
+++ aula2/src/components/Card.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 
 function Card ({tituloDoCard}) {
+
+    const [valor, setValor] = useState(0);
+
+    const diminuir = () => {
+        if(valor > 0) {
+            setValor(valor - 1)
+        }
+    }
+
     return (
-        <div className="input-group"><label for="men">{tituloDoCard}</label><input type="number" id="men" value="0" className="input-valid"/>
+        <div className="input-group"><label for="men">{tituloDoCard}</label><input type="number" id="men" value={valor} className="input-valid"/>
             <div className="button-group">
-                <button className="input-group__button--small">-</button>
-                <button className="input-group__button--small">+</button>
+                <button className="input-group__button--small" onClick={diminuir} >-</button>
+                <button className="input-group__button--small" onClick={() => setValor(valor + 1)}>+</button>
             </div>
         </div>
     );
 }
 
+
 export default Card;
+
 
 
